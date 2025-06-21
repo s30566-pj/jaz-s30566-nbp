3317346d883381ac0f60127e0cbff0ab2dee8fd5
Daniel Makowski <s30566@pjwstk.edu.pl>
2025-06-21 10:18:39 +0200

    Init

---
+++ src/main/java/pl/pjatk/jaz_s30566_nbp/JazS30566NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jaz_s30566_nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS30566NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS30566NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/jaz_s30566_nbp/JazS30566NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jaz_s30566_nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS30566NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
